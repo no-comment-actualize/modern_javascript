bc2f12d5509d1846a507812215eb7cf5bfe7a240
dani <dani.zaghian@gmail.com>
2019-09-26 10:26:11 -0700

    adds mathjs library and require syntax

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+var moment = require('moment');
+var math = require('mathjs');
+
 console.log("hello from js!");
 
 var relativeDay = moment().startOf('day').fromNow();
 console.log(relativeDay);
+
+console.log(math.sqrt(-4));
